d2b0dba6e703da07d30c769d6d97a908d7481b74
Rohan Janardhan <rohanjanardhan30@gmail.com>
2023-12-20 21:45:49 +0530

    Setting up Peerjs

---
+++ hooks/usePeer.js
@@ -0,0 +1,28 @@
+const { useState, useEffect, useRef } = require("react")
+
+const usePeer = () => {
+    const [peer,setPeer] = useState(null)
+    const [myId,setMyId] = useState('')
+    const isPeerSet = useRef(false)
+
+    useEffect(() => {
+        if (isPeerSet.current) return;
+        isPeerSet.current = true
+        (async function initPeer(){
+            const myPeer = new (await import('peerjs').default())
+            setPeer(myPeer)
+
+            myPeer.on('open', (id) => {
+                console.log(`your peer is is ${id}`)
+                setMyId(id)
+            })
+        })()
+    },[])
+
+    return(
+        peer,
+        myId
+    )
+}
+
+export default usePeer;
