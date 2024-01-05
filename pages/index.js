5ddb5f4bda8de2930f822d22a44100bce921fcd0
Rohan Janardhan <rohanjanardhan30@gmail.com>
2024-01-05 13:09:14 +0530

    Creating Lobby

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
+import {v4 as uuidv4} from 'uuid'
+import { useRouter } from "next/navigation"
+
+import styles from '@/styles/home.module.css'
+
 export default function Home() {
+  const router = useRouter()
+
+  const createAndJoin = () => {
+    const roomId = uuidv4()
+    router.push(`/${roomId}`)
+  }
   return (
-    <h1>Welcome</h1>    
+    <div>
+      <h1>Google Meet Clone</h1>
+      <div>
+        <input />
+        <button>Join Room</button>
+      </div>
+      <span>--------------- OR ---------------</span>
+      <button>Create Room</button>
+    </div> 
   )
 }
