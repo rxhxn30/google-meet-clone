98dc5ede3bf365bc769efad06d200f255b3490d2
Rohan Janardhan <rohanjanardhan30@gmail.com>
2024-01-05 14:21:41 +0530

    Lobby Styling

--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,32 @@ import {v4 as uuidv4} from 'uuid'
 import { useRouter } from "next/navigation"
 
 import styles from '@/styles/home.module.css'
+import { useState } from 'react'
 
 export default function Home() {
   const router = useRouter()
+  const [roomId,setRoomId] = useState('')
 
   const createAndJoin = () => {
     const roomId = uuidv4()
     router.push(`/${roomId}`)
   }
+
+  const joinRoom = () => {
+    if(roomId) router.push(`/${roomId}`)
+    else{
+      alert('Please enter a valid room ID')}
+  }
+
   return (
-    <div>
+    <div className={styles.homeContainer}>
       <h1>Google Meet Clone</h1>
-      <div>
-        <input />
-        <button>Join Room</button>
+      <div className={styles.enterRoom}>
+        <input placeholder='Enter Room ID' value={roomId} onChange={(e) => setRoomId(e?.target?.value)}/>
+        <button onClick={joinRoom}>Join Room</button>
       </div>
-      <span>--------------- OR ---------------</span>
-      <button>Create Room</button>
+      <span className={styles.seperatorText}>--------------- OR ---------------</span>
+      <button onClick={createAndJoin}>Create Room</button>
     </div> 
   )
 }
